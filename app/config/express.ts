9af9b24a5ec322be0aad76cb975f0eeb2ba08a43
chiragjadhav22 <93976867+chiragjadhav22@users.noreply.github.com>
2021-12-17 18:43:34 +0530

    Update express.ts

--- app/config/express.ts
+++ app/config/express.ts
@@ -10,7 +10,16 @@ import joiErrorHandler from '../Middlewares/joiErrorHandler';
 import Authenticate from '../Middlewares/Authenticate';
 
 const app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token_access, user_id, User-agent");
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+});
 require('dotenv').config();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
