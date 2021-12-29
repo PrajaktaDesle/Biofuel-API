cd58c610eed708cf1be2e46f1f2020d6ccede980
chiragjadhav22 <93976867+chiragjadhav22@users.noreply.github.com>
2021-12-29 11:49:18 +0530

    Update AfterInstallHook.sh

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -3,5 +3,16 @@ set -e
 cd /home/ubuntu/Nidhi-Bank-API
 npm install
 npm run build
+# Kill Pm2 Process
+pm2 kill
+echo "Kill All Pm2 Process"
+# Start PM2
+pm2 start ecosystem.config.js 2>&1
+if [ $? != "0" ]; then
+   cat /var/log/deployment-logs/bootscript-log.log;
+   error_exit "pm2 start unsuccessful"
+else
+   echo "PM2 started";
+fi
 #pm2 stop all --silent 
 #pm2 start ecosystem.config.js
