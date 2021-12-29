7ba7f1593a1dfe1a40eff24e16967f045e7504cb
chiragjadhav22 <93976867+chiragjadhav22@users.noreply.github.com>
2021-12-29 14:41:25 +0530

    Update AfterInstallHook.sh

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -7,7 +7,7 @@ npm run build
 pm2 kill
 echo "Kill All Pm2 Process"
 # Stop all PM2
-pm2 stop all
+# pm2 stop all
 echo "PM2 Stop All Succesfull"
 #pm2 stop all --silent 
 #pm2 start ecosystem.config.js
