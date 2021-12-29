17afea736f3c1e051d31df9242337267a189ffad
chiragjadhav22 <93976867+chiragjadhav22@users.noreply.github.com>
2021-12-29 14:38:18 +0530

    Update AfterInstallHook.sh

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -9,13 +9,5 @@ echo "Kill All Pm2 Process"
 # Stop all PM2
 pm2 stop all
 echo "PM2 Stop All Succesfull"
-# Start PM2
-pm2 start ecosystem.config.js 2>&1
-if [ $? != "0" ]; then
-   cat /var/log/deployment-logs/bootscript-log.log;
-   error_exit "pm2 start unsuccessful"
-else
-   echo "PM2 started";
-fi
 #pm2 stop all --silent 
 #pm2 start ecosystem.config.js
