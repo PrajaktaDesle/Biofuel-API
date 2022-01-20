5b9e0e44b7578afcaf8d6174405fd71f4cc98702
chiragjadhav22 <93976867+chiragjadhav22@users.noreply.github.com>
2022-01-20 12:36:49 +0530

    Update BeforeInstallHook.sh

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -2,4 +2,6 @@
 set -e
 sudo npm update -y
 sudo pm2 update
-sudo aws s3 cp s3://code-deploynidhiapi/secrets/ecosystem.config.js /home/ubuntu/Nidhi-Bank-API/
+sudo aws s3 cp s3://code-deploynidhiapi/secrets/QA-Nidhi-Api/ecosystem.config.js /home/ubuntu/Nidhi-Bank-API/
+sudo aws s3 cp s3://code-deploynidhiapi/secrets/QA-Nidhi-Api/default /etc/nginx/sites-enabled/
+sudo service nginx reload
