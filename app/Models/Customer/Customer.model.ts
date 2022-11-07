9127b2355da373a00afbb775cecdde6dbcb23174
sagarkanke <sagar@digitalflake.com>
2022-11-07 12:23:20 +0530

    unwanted changes removed

--- app/Models/Customer/Customer.model.ts
+++ app/Models/Customer/Customer.model.ts
@@ -8,21 +8,21 @@ export class CustomerModel extends BaseModel
         super();
     }
     async getCustomer(mobile:string){
-        return await this._executeQuery("select * from customer where mobile = ?", [mobile]);
+        return await this._executeQuery("select * from user where mobile = ?", [mobile]);
     }
     async createCustomer(customerData:any){
-        return await this._executeQuery("insert into customer set ?", [customerData]);
+        return await this._executeQuery("insert into user set ?", [customerData]);
     }
   
     async findAllcustomers(){
-        return await this._executeQuery("select * from customer", []);
+        return await this._executeQuery("select * from user", []);
 
     }
     async findCustomerById(id: any ){
-        return await this._executeQuery("select * from customer where id = ? ", [id]);
+        return await this._executeQuery("select * from user where id = ? ", [id]);
     }
 
     async updateCustomerDetails(data:any){
-        return await this._executeQuery("update customer set ? where id = ? ", [data, data.id]);
+        return await this._executeQuery("update user set ? where id = ? ", [data, data.id]);
     }
 }
