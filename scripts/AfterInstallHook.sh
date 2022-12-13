b5bde023316b4e69cf892bf60f74a9d1d0361b16
chiragjadhav <chirag@digitalflake.com>
2022-12-13 15:34:48 +0530

    CHanges in Scripts of deployment

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-cd /home/ubuntu/Nidhi-Bank-API
-sudo aws s3 cp s3://biofuel-s3/secrets/Dev-Api/.env /home/ubuntu/Biofuel-API/
+cd /home/ubuntu/Biofuel-API
 npm install
 npm run build
 # Kill Pm2 Process
@@ -11,4 +10,4 @@ echo "Kill All Pm2 Process"
 # pm2 stop all
 echo "PM2 Stop All Succesfull"
 #pm2 stop all --silent 
-#pm2 start ecosystem.config.js
+pm2 start pm2.config.js
--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -2,5 +2,5 @@
 set -e
 sudo npm update -y
 sudo pm2 update
-#sudo aws s3 cp s3://biofuel-s3/secrets/Dev-Api/.env /home/ubuntu/Biofuel-API/
+sudo aws s3 cp s3://biofuel-s3/secrets/Dev-Api/.env /home/ubuntu/Biofuel-API/
 #sudo service nginx reload
