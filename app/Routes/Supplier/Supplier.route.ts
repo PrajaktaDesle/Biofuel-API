be6b2aff4aedc37c510bae22a0a6addddfa8d76b
Digitalflake Sagar <digitalflakesagar@Macs-MacBook-Air.local>
2022-12-21 18:22:45 +0530

    code

--- app/Routes/Supplier/Supplier.route.ts
+++ app/Routes/Supplier/Supplier.route.ts
@@ -56,14 +56,14 @@ router.put(
   celebrate(supplierSchema.updateSupplierPOStatus),
   supplierController.updateSupplierPO
 )
-router.post(
-    '/create/delivery-challan',
-        supplierController.createDeliveryChallan
-)
-router.post(
-    '/fetch/all/challan',
-    supplierController.fetchAllDeliveryChallan
-)
+// router.post(
+//     '/create/delivery-challan',
+//         supplierController.createDeliveryChallan
+// )
+// router.post(
+//     '/fetch/all/challan',
+//     supplierController.fetchAllDeliveryChallan
+// )
 
 router.get(
   '/fetch/list',
