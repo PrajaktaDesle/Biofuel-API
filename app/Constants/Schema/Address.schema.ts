8378c0c2e97044b552a41788c3c317017d3d6a86
digitalflakesagar <sagar@digitalfalke.com>
2023-01-12 12:46:57 +0530

    getCitiesByState request is changed to a post request

--- app/Constants/Schema/Address.schema.ts
+++ app/Constants/Schema/Address.schema.ts
@@ -2,7 +2,7 @@ import { Joi, Segments } from 'celebrate';
 export default {
     
     citiesByState: { 
-        [Segments.QUERY]: {
+        [Segments.BODY]: {
             state_id: Joi.number().required(),
             key : Joi.any().optional(),
         }
